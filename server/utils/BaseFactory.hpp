9592abf67cc737368c8b8641fa1cdf70c6429374
Alexandre Tea <alexandre.qtea@gmail.com>
2015-12-07 12:24:22 +0100

    misc to utils ; basefactory OK + ut

---
+++ server/utils/BaseFactory.hpp
@@ -0,0 +1,108 @@
+#ifndef BASEFACTORY_HPP_
+# define BASEFACTORY_HPP_
+
+# include <memory>
+# include <unordered_map>
+# include <utility>
+# include <string>
+# include <sstream>
+# include "IClonable.hpp"
+# include "IStringable.hpp"
+
+namespace Utils
+{
+    /*
+    ** BaseFactory
+    ** Singleton design pattern
+    ** Factory that can learn/unlearn types and then generates them
+    ** Be careful, Type has to be able to use the clone member function /!\
+    */
+    template <typename Type>
+    class BaseFactory : public IStringable
+    {
+    public:
+        typedef std::unique_ptr<BaseFactory>        UniqueFactoryPtr;
+        typedef std::unique_ptr<Type>               UniqueTypePtr;
+        typedef std::unordered_map<std::string,
+                                    UniqueTypePtr>  TypeBook;
+        typedef std::pair<std::string,
+                            UniqueTypePtr>          TypeEntry;
+
+    protected:
+        BaseFactory()
+        {}
+
+    public:
+        virtual ~BaseFactory()
+        {}
+
+    public:
+        static BaseFactory&     getInstance()
+        {
+            if (instance == nullptr)
+                instance = std::unique_ptr<BaseFactory>(new BaseFactory());
+            return *instance;
+        }
+
+    public:
+        bool                    learn(UniqueTypePtr type)
+        {
+            if (type == nullptr)
+                return false;
+            return _book.insert(
+                std::make_pair(type->getName(),
+                                std::unique_ptr<Type>(type->clone()))).second;
+        }
+
+        bool                    unlearn(std::string const& name)
+        {
+            return _book.erase(name) > 0;
+        }
+
+        Type*                   generate(std::string const& name) const
+        {
+            typename TypeBook::const_iterator      it;
+
+            if ((it = _book.find(name)) == _book.end())
+                return nullptr;
+            return it->second->clone();
+        }
+
+        Type const*             getType(std::string const& name) const
+        {
+            typename TypeBook::const_iterator      it;
+
+            if ((it = _book.find(name)) == _book.end())
+                return nullptr;
+            return it->second.get();
+        }
+
+        void                    clear()
+        {
+            _book.clear();
+        }
+
+    public:
+        virtual std::string     toString() const
+        {
+            std::ostringstream  ss;
+
+            ss << "BaseFactory {"
+                << "\n\ttypes:";
+            for (auto& entry: _book)
+                ss << "\n\t\t" << entry.first;
+            ss << "\n}\n";
+            return ss.str();
+        }
+
+    protected:
+        TypeBook                    _book;
+        static UniqueFactoryPtr     instance;
+    };
+
+    template <typename Type>
+    std::unique_ptr<BaseFactory<Type> >     BaseFactory<Type>::instance = nullptr;
+}
+
+
+#endif /* !BASEFACTORY_HPP_ */
