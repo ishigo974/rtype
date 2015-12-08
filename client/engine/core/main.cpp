461757695d96964a2d6e8e36c0d404975864b892
Maxime Menigoz <maxime.menigoz@gmail.com>
2015-12-08 15:23:08 +0100

    Do all tests in main (except graphical renderer)

--- client/engine/core/main.cpp
+++ client/engine/core/main.cpp
@@ -1,71 +1,74 @@
 #include <SFML/Graphics.hpp>
 #include <assert.h>
+#include <unistd.h>
 #include "GameObject.hpp"
 #include "StateMachine.hpp"
 #include "CommandSystem.hpp"
 #include "Event.hpp"
+#include "EntityManager.hpp"
+#include "Behaviour.hpp"
+#include "Renderer.hpp"
+#include "BigBen.hpp"
 
-//bool gameObjectTest()
-//{
-//    EntityManager entityManager;
-//
-//    GameObject *a = entityManager.createEntity<GameObject>("Test", 1);
-//    assert(a->getId() == 1);
-//    assert(a->getName() == "Test");
-//    assert(a->getLayer() == 1);
-//
-//    GameObject *b = entityManager.createEntity<GameObject>("Test", 2);
-//    assert(b->getId() == 2);
-//    assert(b->getName() == "Test");
-//    assert(b->getLayer() == 2);
-//
-//    entityManager.attachComponent<Behaviour>(a, "Behave");
-//
-//    Behaviour *be = a->getComponent<Behaviour>();
-//    Transform t   = a->transform();
-//
-//    assert(be != nullptr);
-//    assert(be->getName() == "Behave");
-//
-//    assert(t.getPosition().X() == 0);
-//    t.getPosition().setX(100);
-//
-//    std::cout << a->toString() << std::endl;
-//    std::cout << b->toString() << std::endl;
-//
-//    return (true);
-//}
-//
-//void renderTest()
-//{
-//    EntityManager entityManager;
-//    Renderer      r;
-//    GameObject    *a = entityManager.createEntity<GameObject>("Test", 1);
-//
-//    entityManager.attachComponent<Transform>(a, cu::Position(100, 100));
-//    entityManager.attachComponent<SpriteRenderer>(a, "lel", "r-typesheet1.gif", gu::Rect<int>(100, 0, 100, 300));
-//
-//    r.init();
-//
-//    while (1)
-//    {
-//        usleep(10000);
-//        r.draw(*a);
-//        r.render();
-//    }
-//}
-//
-//bool timeTest()
-//{
-//    for (int i = 0; i < 5; ++i)
-//    {
-//        std::cout << "i = " << i << " ; elapsed = " << BigBen::get().getElapsedtime() << std::endl;
-//        std::cout << "i = " << i << " ; fixedElapsed = " << BigBen::get().getFixedElapsedtime() << std::endl;
-//        usleep(5000);
-//    }
-//
-//    return (true);
-//}
+bool gameObjectTest(EntityManager & entityManager)
+{
+   GameObject *a = entityManager.createEntity<GameObject>("Test", 1);
+   assert(a->getId() == 1);
+   assert(a->getName() == "Test");
+   assert(a->getLayer() == 1);
+
+   GameObject *b = entityManager.createEntity<GameObject>("Test", 2);
+   assert(b->getId() == 2);
+   assert(b->getName() == "Test");
+   assert(b->getLayer() == 2);
+
+   entityManager.attachComponent<Behaviour>(a, "Behave");
+
+   Behaviour *be = a->getComponent<Behaviour>();
+   Transform t   = a->transform();
+
+   assert(be != nullptr);
+   assert(be->getName() == "Behave");
+
+   assert(t.getPosition().X() == 0);
+   t.getPosition().setX(100);
+
+   std::cout << a->toString();
+   std::cout << b->toString();
+
+   return (true);
+}
+
+void renderTest()
+{
+   EntityManager entityManager;
+   Renderer      r;
+   GameObject    *a = entityManager.createEntity<GameObject>("Test", 1);
+
+   entityManager.attachComponent<Transform>(a, cu::Position(100, 100));
+   entityManager.attachComponent<SpriteRenderer>(a, "lel", "r-typesheet1.gif", gu::Rect<int>(100, 0, 100, 300));
+
+   r.init();
+
+   while (1)
+   {
+       usleep(10000);
+       r.draw(*a);
+       r.render();
+   }
+}
+
+bool timeTest()
+{
+   for (int i = 0; i < 5; ++i)
+   {
+       std::cout << "i = " << i << " ; elapsed = " << BigBen::get().getElapsedtime() << std::endl;
+       std::cout << "i = " << i << " ; fixedElapsed = " << BigBen::get().getFixedElapsedtime() << std::endl;
+       usleep(5000);
+   }
+
+   return (true);
+}
 
 bool stateMachineTest()
 {
@@ -96,9 +99,9 @@ bool stateMachineTest()
     return (true);
 }
 
-bool commandSystemTest()
+bool commandSystemTest(EntityManager *entityManager)
 {
-  CommandSystem cmds;
+  CommandSystem cmds(entityManager);
   cu::Event upEvent;
   cu::Event downEvent;
 
@@ -109,17 +112,22 @@ bool commandSystemTest()
   cmds.addCommand(downEvent);
   std::cout << cmds.toString() << std::endl;
   assert(cmds.getSize() == 2);
+  cmds.process();
   return (true);
 }
 
 int main()
 {
-//    if (gameObjectTest())
-//        std::cout << "gameObjectTest passed -> OK" << std::endl;
-//    if (timeTest())
-//        std::cout << "timeTest passed -> OK" << std::endl;
-//    renderTest();
-    stateMachineTest();
-    commandSystemTest();
-    return (0);
+  EntityManager entityManager;
+
+  if (gameObjectTest(entityManager))
+    std::cout << "\e[32mgameObjectTest passed -> OK\e[0m" << std::endl << std::endl;
+  if (timeTest())
+    std::cout << "\e[32mtimeTest passed -> OK\e[0m" << std::endl << std::endl;
+  // renderTest();
+  if (stateMachineTest())
+    std::cout << "\e[32mstateMachineTest passed -> OK\e[0m" << std::endl << std::endl;
+  if (commandSystemTest(&entityManager))
+    std::cout << "\e[32mCommandSystem passed -> OK\e[0m" << std::endl;
+  return (0);
 }
