ffb3e6ba52fe17d4608c50e85043d0e72641aeba
fourdr_b <fourdr_b@epitech.eu>
2015-12-16 10:26:47 +0100

    Cleaned

--- client/engine/graphics/Renderer.cpp
+++ client/engine/graphics/Renderer.cpp
@@ -2,42 +2,40 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "Renderer.hpp"
-#include "Transform.hpp"
-#include "SpriteRenderer.hpp"
 
 
 Renderer::Renderer() :
-	_win(sf::VideoMode(1280, 720), "Hey-Type",
-       sf::Style::Titlebar | sf::Style::Close)
+        _win(sf::VideoMode(1280, 720), "Hey-Type",
+             sf::Style::Titlebar | sf::Style::Close)
 {
-	_win.setFramerateLimit(60);
+    _win.setFramerateLimit(60);
 }
 
 void    Renderer::init()
 {
-  this->_res.addTexture("../res/r-typesheet1.gif");
-  this->_res.addTexture("../res/bg1.jpg", true);
-  this->_res.addTexture("../res/bg2.jpg", true);
-  this->_res.addTexture("../res/bg3.jpg", true);
-  this->_res.addTexture("../res/bg4.jpg", true);
-  this->_res.addTexture("../res/menu1.jpg");
-  this->_res.addTexture("../res/menu2.jpg");
-  this->_res.addTexture("../res/menu3.jpg");
-  this->_res.addTexture("../res/menu4.jpg");
-  this->_res.addTexture("../res/menu5.jpg");
-  this->_res.addTexture("../res/menu6.jpg");
-  this->_res.addTexture("../res/mob.gif", true);
-  this->_res.addTexture("../res/player.gif", true);
+    this->_res.addTexture("../res/r-typesheet1.gif");
+    this->_res.addTexture("../res/bg1.jpg", true);
+    this->_res.addTexture("../res/bg2.jpg", true);
+    this->_res.addTexture("../res/bg3.jpg", true);
+    this->_res.addTexture("../res/bg4.jpg", true);
+    this->_res.addTexture("../res/menu1.jpg");
+    this->_res.addTexture("../res/menu2.jpg");
+    this->_res.addTexture("../res/menu3.jpg");
+    this->_res.addTexture("../res/menu4.jpg");
+    this->_res.addTexture("../res/menu5.jpg");
+    this->_res.addTexture("../res/menu6.jpg");
+    this->_res.addTexture("../res/mob.gif", true);
+    this->_res.addTexture("../res/player.gif", true);
 }
 
 void Renderer::render()
 {
-	this->_win.display();
+    this->_win.display();
 }
 
 sf::RenderWindow& Renderer::getWindow()
 {
-	return _win;
+    return _win;
 }
 
 void Renderer::draw(const GameObject& object)
@@ -46,7 +44,7 @@ void Renderer::draw(const GameObject& object)
     Transform      tr = object.transform();
     sf::Sprite     sprite;
 
-	// this->_win.clear(sf::Color::White);
+    // this->_win.clear(sf::Color::White);
     sprite.setTexture(*this->_res[sr.getPath()]);
     sprite.setTextureRect(sf::IntRect(sr.getRect().x,
                                       sr.getRect().y,
