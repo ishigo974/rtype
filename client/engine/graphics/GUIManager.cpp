da99beceb8d3409cfa595a4025bfb57c9d25120b
Flff <samy.kettani@gmail.com>
2015-12-19 12:46:34 +0100

    WIP GUI Manager

--- client/engine/graphics/GUIManager.cpp
+++ client/engine/graphics/GUIManager.cpp
@@ -1,7 +1,28 @@
 #include "GUIManager.hpp"
 
 GUIManager::GUIManager(unsigned int id) :
-  _id(id)
+    _id(id)
 {}
 
 GUIManager::~GUIManager() {}
+
+void    GUIManager::addGUIElement(std::string const& state, GUIElement *gui)
+{
+    this->_scenes[state];
+    this->_scenes[state].push_back(gui);
+}
+
+void    GUIManager::draw(sf::RenderWindow& win, std::string const& state)
+{
+    for (auto it = this->_scenes[state].begin();
+        it != this->_scenes[state].end();
+        ++it)
+    {
+        (*it)->draw(win);
+    }
+}
+
+RTypes::my_uint16_t GUIManager::getMask() const
+{
+    return Mask;
+}
--- client/engine/graphics/GUIManager.hpp
+++ client/engine/graphics/GUIManager.hpp
@@ -1,16 +1,27 @@
 #ifndef GUIMANAGER_HPP_
 # define GUIMANAGER_HPP_
 
+# include <unordered_map>
 # include "Component.hpp"
+# include "GUIElement.hpp"
 
 class GUIManager : public Component
 {
 public:
-  GUIManager(unsigned int id);
-  virtual ~GUIManager();
+    static const RTypes::my_uint16_t Mask = ComponentMask::GUIManagerMask;
+
+public:
+    GUIManager(unsigned int id);
+    virtual ~GUIManager();
+
+public:
+    void addGUIElement(std::string const& state, GUIElement *gui);
+    void draw(sf::RenderWindow&, std::string const& state);
+    virtual RTypes::my_uint16_t getMask() const;
 
 private:
-  unsigned int	_id;
+    unsigned int	_id;
+    std::unordered_map<std::string, std::vector<GUIElement *> > _scenes;
 };
 
 #endif // !GUIMANAGER_HPP_
