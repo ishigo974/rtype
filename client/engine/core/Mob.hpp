64ed1654757e7450f5f46d56cee3338996d28af8
Alexandre Tea <alexandre.qtea@gmail.com>
2015-12-22 21:22:36 +0100

    pop mobs

--- client/engine/core/Mob.hpp
+++ client/engine/core/Mob.hpp
@@ -3,40 +3,54 @@
 
 # include "Behaviour.hpp"
 # include "Transform.hpp"
+# include "IMobType.hpp"
 
 class Mob : public Behaviour
 {
 public:
     Mob();
-    Mob(unsigned int _id, std::string const& _name, int hp = 1, int damage = 5, int type = 0);
+    Mob(RType::MobType::IMobType const* mobtype);
     virtual ~Mob();
 
+public:
     Mob(Mob const& other);
     Mob(Mob&& other);
     Mob& operator=(Mob other);
 
+public:
     bool operator==(Mob const& other);
     bool operator!=(Mob const& other);
 
-    int getHp() const;
-    virtual int getDamage() const;
+public:
+    void            swap(Mob& other);
+    virtual void    update(double);
+    virtual bool    handleMessage(Collider *o);
 
-    void         move();
-    virtual void update(double);
+public:
+    void            init(RType::MobType::IMobType const* mobtype);
+    void            addLives(unsigned int nb = 1);
+    void            removeLives(unsigned int nb = 1);
 
-    void swap(Mob& other);
-
-    virtual bool handleMessage(Collider *o);
-
-    std::string	toString() const;
+public:
+    unsigned int                    getId() const;
+    std::string const&              getMobName() const;
+    unsigned int                    getLives() const;
+    unsigned int                    getScoreValue() const;
+    std::string const&              getSpriteFilePath() const;
+    RType::MobType::MovePattern const&     getMovePattern() const;
+    std::string                     toString() const;
 
 protected:
-    int _hp;
-    int _damage;
-    int _direction;
-    int	_type;
-    int	_graphicHeight;
-    Transform	*_transform;
+    void                    move(double elapsedTime);
+
+protected:
+    unsigned int            _id;
+    std::string             _name;
+    unsigned int            _lives;
+    unsigned int            _scoreValue;
+    std::string             _spriteFilePath;
+    RType::MobType::MovePattern    _movePattern;
+    Transform*              _transform;
 };
 
 #endif /* !MOB_HPP_ */
