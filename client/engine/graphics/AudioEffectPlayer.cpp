efa1c7d9effdf35d25564f735e324e034a3c4595
Denis Le Borgne <denis.le-borgne@epitech.eu>
2015-12-23 01:23:45 +0100

    patch audio

--- client/engine/graphics/AudioEffectPlayer.cpp
+++ client/engine/graphics/AudioEffectPlayer.cpp
@@ -4,15 +4,25 @@
 
 void    AudioEffectPlayer::play(GameObject *obj)
 {
+    for (std::list<sf::Sound>::iterator it = _sounds.begin();
+         it != _sounds.end();)
+    {
+        if (it->getStatus() != sf::Sound::Playing)
+            it = _sounds.erase(it);
+        else
+            ++it;
+    }
     try
     {
-        if (!_sounds.empty()
+        if ((!_sounds.empty()
             && _sounds.begin()->getStatus() == sf::Sound::Stopped
-            && _backgroundStart)
+            && _backgroundStart) || _sounds.empty())
         {
             obj->getComponent<AudioEffect>()->setSoundToPlay("../res/music.wav");
+            _sounds.push_front(obj->getComponent<AudioEffect>()->soundToPlay());
         }
-        _sounds.push_back(obj->getComponent<AudioEffect>()->soundToPlay());
+        else
+            _sounds.push_back(obj->getComponent<AudioEffect>()->soundToPlay());
     }
     catch (Exception::NotImplemented const&e)
     {
@@ -23,14 +33,6 @@ void    AudioEffectPlayer::play(GameObject *obj)
         _sounds.back().setVolume(0.5);
     _sounds.back().play();
     obj->getComponent<AudioEffect>()->clearSounds();
-    for (std::list<sf::Sound>::iterator it = _sounds.begin();
-         it != _sounds.end();)
-    {
-        if (it->getStatus() != sf::Sound::Playing)
-            it = _sounds.erase(it);
-        else
-            ++it;
-    }
     _backgroundStart = true;
 }
 
