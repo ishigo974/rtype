1bed3e9c827c0645de678223fe058fdc29c8d5f7
Flff <samy.kettani@gmail.com>
2015-12-25 18:34:06 +0100

    fix

--- client/engine/core/RTypeGame.cpp
+++ client/engine/core/RTypeGame.cpp
@@ -107,7 +107,7 @@ void        RTypeGame::initGameSample()
     player->init();
     GameObject *bg = _em.createEntity<GameObject>("bg", -10);
     GameObject *ds = _em.createEntity<GameObject>("ds", -5);
-    GameObject *df = _em.createEntity<GameObject>("df", -4);
+    // GameObject *df = _em.createEntity<GameObject>("df", -4);
     GameObject *opm = _em.createEntity<GameObject>("opm", -3);
     GameObject *pr = _em.createEntity<GameObject>("pr", 2);
     AudioEffect*    audio;
@@ -116,19 +116,19 @@ void        RTypeGame::initGameSample()
         throw std::runtime_error("No mobs types loaded");
 
     _em.attachComponent<SpriteRenderer>(ds, "ds", "deathstar", gu::Rect<int>(0, 0, 1280, 720));
-    _em.attachComponent<ScrollingBackground>(ds, "DeathStar", 0.22);
-
-    _em.attachComponent<SpriteRenderer>(df, "df", "dogfight", gu::Rect<int>(0, 0, 1280, 720));
-    _em.attachComponent<ScrollingBackground>(df, "Background", 0.27);
+    _em.attachComponent<ScrollingBackground>(ds, "DeathStar", 0.3);
+    //
+    // _em.attachComponent<SpriteRenderer>(df, "df", "dogfight", gu::Rect<int>(0, 0, 1280, 720));
+    // _em.attachComponent<ScrollingBackground>(df, "Background", 0.3);
 
     _em.attachComponent<SpriteRenderer>(bg, "bg", "bg1", gu::Rect<int>(0, 0, 1280, 720));
-    _em.attachComponent<ScrollingBackground>(bg, "Background", 0.3);
+    _em.attachComponent<ScrollingBackground>(bg, "Background", 0.27);
 
     _em.attachComponent<SpriteRenderer>(opm, "opm", "opm", gu::Rect<int>(0, 0, 1280, 720));
-    _em.attachComponent<ScrollingBackground>(opm, "OPM", 0.50);
+    _em.attachComponent<ScrollingBackground>(opm, "OPM", 0.55);
 
     _em.attachComponent<SpriteRenderer>(pr, "pr", "pr1", gu::Rect<int>(0, 0, 1280, 720));
-    _em.attachComponent<ScrollingBackground>(pr, "Paralax", 0.40);
+    _em.attachComponent<ScrollingBackground>(pr, "Paralax", 0.60);
 
     _em.attachComponent<AudioEffect>(player, "Audio");
 
