f24cad7495e73ebaf9e1e32e54d6f9381e5d98fa
Cédric Voinnet <cedric.voinnet@epitech.eu>
2015-12-25 08:52:13 +0100

    Compile on windows

--- common/network/Windows/NetTypes.hpp
+++ common/network/Windows/NetTypes.hpp
@@ -3,13 +3,17 @@
 //
 
 #ifndef RTYPE_SERVER_NETTYPES_HPP
-#define RTYPE_SERVER_NETTYPES_HPP
+# define RTYPE_SERVER_NETTYPES_HPP
 
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <Windows.h>
+# ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+# endif
 
-#pragma comment(lib,"ws2_32")
+# include <WinSock2.h>
+# include <WS2tcpip.h>
+# include <Windows.h>
+
+# pragma comment(lib,"ws2_32")
 
 typedef SOCKET rSocket;
 
