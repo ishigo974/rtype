8f5f0f3d33a472c019d15696b27821f40ce390b9
Denis Le Borgne <denis.le-borgne@epitech.eu>
2015-12-27 23:09:37 +0100

    Add map

---
+++ common/mob_types/build/CMakeFiles/rtype_forwardmob.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtype_forwardmob.dir/ForwardMobType.cpp.o"
+  "CMakeFiles/rtype_forwardmob.dir/Users/Denis/rtype/common/utils/Utils.cpp.o"
+  "librtype_forwardmob.pdb"
+  "librtype_forwardmob.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtype_forwardmob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ common/mob_types/build/CMakeFiles/rtype_verticalmob.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtype_verticalmob.dir/VerticalMobType.cpp.o"
+  "CMakeFiles/rtype_verticalmob.dir/Users/Denis/rtype/common/utils/Utils.cpp.o"
+  "librtype_verticalmob.pdb"
+  "librtype_verticalmob.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtype_verticalmob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
