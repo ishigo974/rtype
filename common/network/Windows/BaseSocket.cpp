7f8766127c88a0532a1526e46fab24fec58701c8
Denis Le Borgne <denis.le-borgne@epitech.eu>
2015-12-27 19:26:55 +0100

    Change WSA windows

--- common/network/Windows/BaseSocket.cpp
+++ common/network/Windows/BaseSocket.cpp
@@ -1,3 +1,4 @@
+
 //
 // Created by Denis Le Borgne on 14/12/2015.
 //
@@ -15,7 +16,6 @@ BaseSocket::~BaseSocket()
 
 size_t    BaseSocket::send(Buffer const& buffer) const
 {
-    struct sockaddr_in client;
     WSABUF             toSend;
     DWORD              SendBytes;
     size_t             ret;
@@ -23,16 +23,11 @@ size_t    BaseSocket::send(Buffer const& buffer) const
 
     toSend.len             = buffer.size();
     toSend.buf             = buff.data();
-    client.sin_family      = AF_INET;
-    client.sin_addr.s_addr = inet_pton(AF_INET, _addr.c_str(), &client.sin_addr);
-    client.sin_port        = _port;
 
-	if ((ret = WSASendTo(_socket, &toSend, 1, &SendBytes, 0,
-		reinterpret_cast<SOCKADDR *>(&client), sizeof(client),
-		nullptr, nullptr))
+	if ((ret = ::WSASend(_socket, &toSend, 1, &SendBytes, 0, nullptr, nullptr))
 		== SOCKET_ERROR)
 		throw std::runtime_error("WSASend failed");
-	return ret;
+	return SendBytes;
 }
 
 size_t    BaseSocket::receive(Buffer& buffer, size_t len) const
@@ -40,15 +35,11 @@ size_t    BaseSocket::receive(Buffer& buffer, size_t len) const
     WSABUF             wsabuf;
     DWORD              read_size  = 0;
     DWORD              flags      = 0;
-    struct sockaddr_in client;
-    int                clientSize = sizeof(client);
 
     wsabuf.buf = new char[len];
     wsabuf.len = len;
-    if (::WSARecvFrom(_socket, &wsabuf, 1, &read_size, &flags,
-                      reinterpret_cast<SOCKADDR *>(&client), &clientSize,
-                      nullptr, nullptr)
-        == SOCKET_ERROR)
+    if (::WSARecv(_socket, &wsabuf, 1, &read_size, &flags, nullptr, nullptr)
+                                                            == SOCKET_ERROR)
     {
         delete wsabuf.buf;
         throw std::runtime_error("WSARecv failed");
